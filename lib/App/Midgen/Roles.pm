61c8e11e15413ed9b5e2527b0ec0ca86775c18d5
bowtie <bowtie@cpan.org>
2013-02-05 17:18:11 +0000

    first attempt at a Role with the GetOpps Long cmd varibles

---
+++ lib/App/Midgen/Roles.pm
@@ -0,0 +1,51 @@
+package App::Midgen::Roles;
+
+use v5.10;
+use strict;
+use warnings;
+
+our $VERSION = '0.05';
+use English qw( -no_match_vars ); # Avoids regex performance penalty
+local $OUTPUT_AUTOFLUSH = 1;
+
+use Moo::Role;
+use MooX::Types::MooseLike::Base qw(:all);
+
+
+has 'base_parent' => (
+	is  => 'ro',
+	isa => Bool,
+	required => 1,
+);
+has 'core' => (
+	is  => 'ro',
+	isa => Bool,
+	required => 1,
+);
+has 'debug' => (
+	is  => 'ro',
+	required => 1,
+	isa => Bool,
+);
+has 'mojo' => (
+	is  => 'ro',
+	isa => Bool,
+	required => 1,
+);
+has 'noisy_children' => (
+	is  => 'ro',
+	isa => Bool,
+	required => 1,
+);
+has 'output_format' => (
+	is => 'ro',
+	isa => Str,
+	required => 1,
+);
+has 'verbose' => (
+	is  => 'ro',
+	isa => Bool,
+	required => 1,
+);
+
+1;
