b79087e1eed97aaf75fc421e91bea75ed1e388f2
bowtie <bowtie@cpan.org>
2013-02-13 16:42:52 +0000

    add pod

--- lib/App/Midgen/Output.pm
+++ lib/App/Midgen/Output.pm
@@ -261,3 +261,115 @@ sub footer_dzil {
 
 __END__
 
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+App::Midgen::Output - Package Options and Attributes used by L<App::Midgen>
+
+=head1 VERSION
+
+This document describes App::Midgen::Output version 0.08
+
+=head1 METHODS
+
+=over 4
+
+=item * header_dsl
+
+=item * body_dsl
+
+=item * footer_dsl
+
+=item * header_mi
+
+=item * body_mi
+
+=item * footer_mi
+
+=item * header_build
+
+=item * body_build
+
+=item * footer_build
+
+=item * header_dzil
+
+=item * body_dzil
+
+=item * footer_dzil
+
+=back
+
+for more info see L<midgen>
+
+=head1 ACCESSORS
+
+=over 4
+
+=item * package_name
+
+Our best guess as to this packages name
+
+=item * package_names
+
+Some package names we found along the way
+
+=item * requires
+
+Some where to store required modules and version info in
+
+=item * test_requires
+
+Some where to store test_required modules and version info in
+
+=back
+
+
+=head1 AUTHOR
+
+Kevin Dawson E<lt>bowtie@cpan.orgE<gt>
+
+=head2 CONTRIBUTORS
+
+none at present
+
+=head1 COPYRIGHT
+
+Copyright E<copy> 2013 AUTHOR and CONTRIBUTORS as listed above.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
+
+=head1 SEE ALSO
+
+L<App::Midgen>,
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
