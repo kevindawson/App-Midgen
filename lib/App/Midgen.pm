809476d4288695693f00cdad60cd0f69ca6e5a3b
bowtie <bowtie@cpan.org>
2013-02-13 16:15:49 +0000

    remove unwanted code

--- lib/App/Midgen.pm
+++ lib/App/Midgen.pm
@@ -555,45 +555,6 @@ sub output_main_body {
 		}
 	}
 
-
-
-	# print "\n";
-
-	# my $pm_length = 0;
-	# foreach my $module_name ( sort keys %{$required_ref} ) {
-		# if ( length $module_name > $pm_length ) {
-			# $pm_length = length $module_name;
-		# }
-	# }
-
-	# say $title . ' => {' if $self->{output_format} eq 'build';
-
-	# foreach my $module_name ( sort keys %{$required_ref} ) {
-		# given ( $self->{output_format} ) {
-			# when ('mi') {
-				# if ( $module_name =~ /^Win32/sxm ) {
-					# my $sq_key = "'$module_name'";
-					# printf "%s %-*s => '%s' if win32;\n", $title, $pm_length + 2, $sq_key,
-						# $required_ref->{$module_name};
-				# } else {
-					# my $sq_key = "'$module_name'";
-					# printf "%s %-*s => '%s';\n", $title, $pm_length + 2, $sq_key, $required_ref->{$module_name};
-				# }
-			# }
-			# when ('dsl') {
-				# if ( $module_name =~ /^Win32/sxm ) {
-					# printf "%s %-*s %s if win32\n", $title, $pm_length, $module_name, $required_ref->{$module_name};
-				# } else {
-					# printf "%s %-*s %s\n", $title, $pm_length, $module_name, $required_ref->{$module_name};
-				# }
-			# }
-			# when ('build') {
-				# my $sq_key = "'$module_name'";
-				# printf "\t %-*s => '%s',\n", $pm_length + 2, $sq_key, $required_ref->{$module_name};
-			# }
-		# }
-	# }
-	# say '},' if $self->{output_format} eq 'build';
 	return;
 }
 #######
