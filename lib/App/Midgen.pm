1b62b70b0c0ca131415f82e3070ba49e57f24ccc
bowtie <bowtie@cpan.org>
2013-03-22 11:41:22 +0000

    Bump version 0.17_03

--- lib/App/Midgen.pm
+++ lib/App/Midgen.pm
@@ -8,7 +8,7 @@ use App::Midgen::Output;
 # Load time and dependencies negate execution time
 # use namespace::clean -except => 'meta';
 
-our $VERSION = '0.17_02';
+our $VERSION = '0.17_03';
 use English qw( -no_match_vars ); # Avoids reg-ex performance penalty
 local $OUTPUT_AUTOFLUSH = 1;
 
@@ -882,7 +882,7 @@ App::Midgen - Check B<requires> & B<test_requires> of your package for CPAN incl
 
 =head1 VERSION
 
-This document describes App::Midgen version: 0.17_02
+This document describes App::Midgen version: 0.17_03
 
 =head1 SYNOPSIS
 
