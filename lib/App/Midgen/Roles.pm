34db9bc438bf7a718ce0b8115bd96e2d9df2e1f7
bowtie <bowtie@cpan.org>
2013-04-29 21:18:38 +0100

    bump version to 0.21_08

--- lib/App/Midgen/Roles.pm
+++ lib/App/Midgen/Roles.pm
@@ -8,7 +8,7 @@ use Data::Printer { caller_info => 1, colored => 1, };
 # Load time and dependencies negate execution time
 # use namespace::clean -except => 'meta';
 
-our $VERSION = '0.21_07';
+our $VERSION = '0.21_08';
 use Carp;
 
 #######
@@ -240,7 +240,7 @@ App::Midgen::Roles - Package Options and Attributes used by L<App::Midgen>
 
 =head1 VERSION
 
-This document describes App::Midgen::Roles version: 0.21_07
+This document describes App::Midgen::Roles version: 0.21_08
 
 =head1 METHODS
 
