b6975eb62f46181ae9bce8551de50225a37f6d18
bowtie <bowtie@cpan.org>
2013-05-08 19:18:44 +0100

    move use_ok into a trait and tweak

--- lib/App/Midgen/Role/UseOk.pm
+++ lib/App/Midgen/Role/UseOk.pm
@@ -2,133 +2,129 @@ package App::Midgen::Role::UseOk;
 
 use v5.10;
 use Moo::Role;
-#use MooX::Types::MooseLike::Base qw(:all);
+
 use Data::Printer { caller_info => 1, colored => 1, };
 
 # Load time and dependencies negate execution time
 # use namespace::clean -except => 'meta';
 
 our $VERSION = '0.21_08';
-
-
+use constant { BLANK => q{ }, NONE => q{}, TWO => 2, THREE => 3, };
 
 
 #######
 # composed method - _xtests_in_single_quote
 #######
-sub _xtests_in_single_quote {
-  my $self = shift;
+sub xtests_use_ok {
+	my $self = shift;
 
-  # Hack for use_ok in test files, Ouch!
-  # Now lets double check the ptq-Single hidden in a test file
-  my $ppi_tqs = $self->ppi_document->find('PPI::Token::Quote::Single');
-  if ($ppi_tqs) {
+	#PPI::Document
+	#  PPI::Statement::Scheduled
+	#    PPI::Token::Word  	'BEGIN'
+	#    PPI::Token::Whitespace  	' '
+	#    PPI::Structure::Block  	{ ... }
+	#      PPI::Token::Whitespace  	'\n'
+	#      PPI::Token::Whitespace  	'\t'
+	#      PPI::Statement
+	#        PPI::Token::Word  	'use_ok'
+	#        PPI::Structure::List  	( ... )
+	#          PPI::Token::Whitespace  	' '
+	#          PPI::Statement::Expression
+	#            PPI::Token::Quote::Single  	''Term::ReadKey''
+	#            PPI::Token::Operator  	','
+	#            PPI::Token::Whitespace  	' '
+	#            PPI::Token::Quote::Single  	''2.30''
 
-    foreach my $include (@{$ppi_tqs}) {
-      my $module = $include->content;
-      $module =~ s/^[']//;
-      $module =~ s/[']$//;
+	my @modules;
+	my @version_strings;
+	my @chunks =
 
-      p $module if $self->debug;
+		map { [ $_->schildren ] }
+		grep { $_->child(0)->literal =~ m{\A(?:BEGIN)\z} }
+		grep { $_->child(0)->isa('PPI::Token::Word') }
+		@{ $self->ppi_document->find('PPI::Statement::Scheduled') || [] };
 
-      $self->_xtests_includes($module);
-    }
-  }
-  return;
-}
-#######
-# composed method - _xtests_in_double_quote
-#######
-sub _xtests_in_double_quote {
-  my $self = shift;
+	foreach my $hunk (@chunks) {
 
-# Now lets double check the ptq-Doubles hidden in a test file - why O why - rtfm pbp
-  my $ppi_tqd = $self->ppi_document->find('PPI::Token::Quote::Double');
-  if ($ppi_tqd) {
+		# looking for use_ok { 'Term::ReadKey' => '2.30' };
+		if ( grep { $_->isa('PPI::Structure::Block') } @$hunk ) {
 
-    # my @modules;
-    foreach my $include (@{$ppi_tqd}) {
-      my $module = $include->content;
-      $module =~ s/^["]//;
-      $module =~ s/["]$//;
+			# hack for List
+			my @hunkdata = @$hunk;
 
-      p $module if $self->debug;
+			foreach my $ppi_sb (@hunkdata) {
+				if ( $ppi_sb->isa('PPI::Structure::Block') ) {
+					foreach my $ppi_s ( @{ $ppi_sb->{children} } ) {
+						if ( $ppi_s->isa('PPI::Statement') ) {
+							p $ppi_s if $self->debug;
+							if ( $ppi_s->{children}[0]->content eq 'use_ok' ) {
+								my $ppi_sl = $ppi_s->{children}[1];
+								foreach my $ppi_se ( @{ $ppi_sl->{children} } ) {
+									if ( $ppi_se->isa('PPI::Statement::Expression') ) {
+										foreach my $element ( @{ $ppi_se->{children} } ) {
+											if (   $element->isa('PPI::Token::Quote::Single')
+												|| $element->isa('PPI::Token::Quote::Double') )
+											{
 
-      $self->_xtests_includes($module);
-    }
-  }
-  return;
-}
+												my $module = $element;
+#												p $module;
+												$module =~ s/^['|"]//;
+												$module =~ s/['|"]$//;
+												if ( $module =~ m/\A[A-Z]/ ) {
+													say 'found module - ' . $module if $self->debug;
+													push @modules, $module;
+												}
+
+											}
 
 
-#######
-# composed method - _xtests_includes
-#######
-sub _xtests_includes {
-  my $self   = shift;
-  my $module = shift;
-  my @modules;
-
-  if ($module =~ /::/ && $module !~ /main/ && !$module =~ /use/) {
-
-    $module =~ s/(\s[\w|\s]+)$//;
-    p $module if $self->debug;
-
-    # if we have found it already ignore it - or - contains ;|=
-    if (not defined $self->{modules}{$module}{location} and $module !~ /[;|=]/)
-    {
-      push @modules, $module;
-    }
-
-  }
-  elsif ($module =~ /::/ && $module =~ /^[use|require]/) {
-
-    $module =~ s/^(use|require)\s+//;
-    $module =~ s/(\s[\s|\w|\n|.|;]+)$//;
-    $module =~ s/\s+([\$|\w|\n]+)$//;
-    $module =~ s/\s+$//;
-    p $module if $self->debug;
-
-    # if we have found it already ignore it - or - contains ;|=
-    if (not defined $self->{modules}{$module}{location} and $module !~ /[;|=]/)
-    {
-      push @modules, $module;
-    }
-  }
-
-  # lets catch -> use Test::Requires { 'Test::Pod' => 1.46 };
-#  elsif ($module =~ /^\w+::\w+/) {
-#    $module =~ s/(\s.+)$//;
-#    p $module if $self->debug;
+#											if (   $element->isa('PPI::Token::Number::Float')
+#												|| $element->isa('PPI::Token::Quote::Single')
+#												|| $element->isa('PPI::Token::Quote::Double') )
+#											{
+#												my $version_string = $element;
 #
-#    if (not defined $self->{modules}{$module}{location} and $module !~ /[;|=]/)
-#    {
-#      push @modules, $module;
-#      if ($self->xtest eq 'test_requires') {
-#        $self->xtest('recommends');
-#      }
-#    }
-#  }
+#												$version_string =~ s/^['|"]//;
+#												$version_string =~ s/['|"]$//;
+#												next if $version_string !~ m/\A[\d|v]/;
+#												if ( $version_string =~ m/\A[\d|v]/ ) {
+#
+#													push @version_strings, $version_string;
+#													say 'found version_string - ' . $version_string
+#														if $self->debug;
+#												}
+#											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-  # if we found a module, process it
-  if (scalar @modules > 0) {
-    if ($self->xtest eq 'test_requires') {
-      $self->_process_found_modules('test_requires', \@modules);
-    }
-    elsif ($self->develop && $self->xtest eq 'test_develop') {
-      $self->_process_found_modules('test_develop', \@modules);
-    }
-    else {
-      $self->_process_found_modules('recommends', \@modules);
-    }
-  }
+	p @modules         if $self->debug;
+	p @version_strings if $self->debug;
 
-  return;
+	# if we found a module, process it with the correct catogery
+	if ( scalar @modules > 0 ) {
+
+		if ( $self->format eq 'cpanfile' ) {
+			# $self->xtest eq 'test_requires' -> t/
+			# $self->xtest eq 'test_develop' -> xt/
+			if ( $self->xtest eq 'test_requires' ) {
+				$self->_process_found_modules( 'test_requires', \@modules );
+			} elsif ( $self->develop && $self->xtest eq 'test_develop' ) {
+				$self->_process_found_modules( 'test_develop', \@modules );
+			}
+		} else {
+			$self->_process_found_modules( 'test_requires', \@modules );
+		}
+	}
+	return;
 }
-
-
-
-
 
 no Moo::Role;
 
@@ -142,94 +138,32 @@ __END__
 
 =head1 NAME
 
-App::Midgen::Roles - Package Options and Attributes used by L<App::Midgen>
+App::Midgen::Roles::UseOk - extra checks for test files, looking
+for methods in use_ok in BEGIN blocks, used by L<App::Midgen>
 
 =head1 VERSION
 
-This document describes App::Midgen::Roles version: 0.21_08
+version: 0.21_08
 
 =head1 METHODS
 
-none as such, but we do have
-
-=head2 OPTIONS
-
 =over 4
 
-=item * core
+=item * xtests_use_ok 
 
-=item * debug
+Checking for the following, extracting module name only.
 
-=item * dual_life
+ BEGIN {
+   use_ok( 'Term::ReadKey', '2.30' );
+   use_ok( 'Term::ReadLine', '1.10' );
+   use_ok( 'Fred::BloggsOne', '1.01' );
+   use_ok( "Fred::BloggsTwo", "2.02" );
+   use_ok( 'Fred::BloggsThree', 3.03 );
+ }
 
-=item * experimental
-
-=item * quiet
-
-=item * format
-
-=item * verbose
-
-0 -> off
-1 -> default
-2 -> show files
-
-=item * zero
+Used to check files in t/ and xt/ directories.
 
 =back
-
-for more info see L<midgen>
-
-=head2 ACCESSORS
-
-=over 4
-
-=item * found_twins
-
-Used as a flag to re-run noisy children after discovery of twins
-
-=item * mcpan
-
-accessor to MetaCPAN::API object
-
-=item * output
-
-accessor to App::Midgen::Output object
-
-=item * package_name
-
-Our best guess as to this packages name
-
-=item * package_names
-
-Some package names we found along the way
-
-=item * ppi_document
-
-I encapsulated this and got a nifty speed increase
-
-=item * recommends
-
-Some where to store recommend modules and version info in
-
-=item * requires
-
-Some where to store required modules and version info in
-
-=item * scanner
-
-accessor to Perl::PrereqScanner object
-
-=item * test_requires
-
-Some where to store test_required modules and version info in
-
-
-=back
-
-=head1 SEE ALSO
-
-L<App::Midgen>,
 
 =head1 AUTHOR
 
