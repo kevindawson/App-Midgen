12d2690f6307c5828c7d808b526fa3874f2194c8
bowtie <bowtie@cpan.org>
2014-02-27 16:43:23 +0000

    another role tweked for 5.8

--- lib/App/Midgen/Role/Output/MIdsl.pm
+++ lib/App/Midgen/Role/Output/MIdsl.pm
@@ -1,17 +1,13 @@
 package App::Midgen::Role::Output::MIdsl;
 
-use v5.10;
 use Moo::Role;
 requires qw( no_index verbose );
-
-# turn off experimental warnings
-no if $] > 5.017010, warnings => 'experimental::smartmatch';
 
 # Load time and dependencies negate execution time
 # use namespace::clean -except => 'meta';
 
 our $VERSION = '0.29_09';
-$VERSION = eval $VERSION; ## no critic
+$VERSION = eval $VERSION;    ## no critic
 
 use English qw( -no_match_vars );    # Avoids reg-ex performance penalty
 local $OUTPUT_AUTOFLUSH = 1;
@@ -25,92 +21,86 @@ use File::Spec;
 # header_dsl
 #######
 sub header_dsl {
-  my $self         = shift;
-  my $package_name = shift // NONE;
-  my $mi_ver       = shift // NONE;
+	my $self         = shift;
+	my $package_name = shift || NONE; # was shift // NONE -> defined $a ? $a : $b
+	my $mi_ver       = shift || NONE; # defined shift ? shift : NONE - don't work as per perl5100delta.pod
 
-  $package_name =~ s{::}{/}g;
+	$package_name =~ s{::}{/}g;
 
-  print "\n";
-  say 'use inc::Module::Install::DSL ' . colored($mi_ver, 'yellow') . q{;};
-  print "\n";
-  if ($package_name ne NONE) {
-    say 'all_from lib/' . $package_name . '.pm';
-    say 'requires_from lib/' . $package_name . '.pm';
-  }
+	print "\nuse inc::Module::Install::DSL "
+		. colored($mi_ver, 'yellow') . ";\n";
+	if ($package_name ne NONE) {
+		print "all_from lib/$package_name.pm\n";
+		print "requires_from lib/$package_name.pm\n";
+	}
 
-  print "\n";
-  return;
+	print "\n";
+	return;
 }
 #######
 # body_dsl
 #######
 sub body_dsl {
-  my $self         = shift;
-  my $title        = shift;
-  my $required_ref = shift || return;
-  say 'perl_version ' . $App::Midgen::Min_Version if $title eq 'requires';
-  print "\n";
+	my $self         = shift;
+	my $title        = shift;
+	my $required_ref = shift || return;
 
-  my $pm_length = 0;
-  foreach my $module_name (sort keys %{$required_ref}) {
-    if (length $module_name > $pm_length) {
-      $pm_length = length $module_name;
-    }
-  }
+	print 'perl_version ' . $App::Midgen::Min_Version . "\n"
+		if $title eq 'requires';
+	print "\n";
 
-  foreach my $module_name (sort keys %{$required_ref}) {
+	my $pm_length = 0;
+	foreach my $module_name (sort keys %{$required_ref}) {
+		if (length $module_name > $pm_length) {
+			$pm_length = length $module_name;
+		}
+	}
 
-    if ($module_name =~ /^Win32/sxm) {
-      printf "%s %-*s %s %s\n", $title, $pm_length, $module_name,
-        $required_ref->{$module_name}, colored('if win32', 'bright_green');
-    }
-    else {
-      printf "%s %-*s %s\n", $title, $pm_length, $module_name,
-        $required_ref->{$module_name};
-    }
-  }
-  return;
+	foreach my $module_name (sort keys %{$required_ref}) {
+
+		if ($module_name =~ /^Win32/sxm) {
+			printf "%s %-*s %s %s\n", $title, $pm_length, $module_name,
+				$required_ref->{$module_name}, colored('if win32', 'bright_green');
+		}
+		else {
+			printf "%s %-*s %s\n", $title, $pm_length, $module_name,
+				$required_ref->{$module_name};
+		}
+	}
+	return;
 }
 #######
 # footer_dsl
 #######
 sub footer_dsl {
-  my $self = shift;
-  my $package_name = shift // NONE;
-  $package_name =~ s{::}{-}g;
+	my $self = shift;
+	my $package_name = shift || NONE;
+	$package_name =~ s{::}{-}g;
 
-  if ($self->verbose > 0) {
-    print BRIGHT_BLACK "\n";
-    say '# ToDo you should consider the following';
-    say "homepage    https://github.com/.../$package_name";
-    say "bugtracker  https://github.com/.../$package_name/issues";
-    say "repository  git://github.com/.../$package_name.git";
-    print CLEAR "\n";
-  }
-  if (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'share')) {
-    say 'install_share';
-    print "\n";
-  }
+	if ($self->verbose > 0) {
+		print BRIGHT_BLACK "\n";
+		print "homepage    https://github.com/.../$package_name\n";
+		print "bugtracker  https://github.com/.../$package_name/issues\n";
+		print "repository  git://github.com/.../$package_name.git\n";
+		print CLEAR "\n";
+	}
+	if (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'share')) {
+		print "install_share\n\n";
+	}
 
-  if (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'script')) {
-    say 'install_script ...';
-    print "\n";
-  }
-  elsif (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'bin')) {
-    say 'install_script bin/...';
-    print "\n";
-  }
+	if (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'script')) {
+		print "install_script ...\n\n";
+	}
+	elsif (defined -d File::Spec->catdir($App::Midgen::Working_Dir, 'bin')) {
+		print "install_script bin/...\n\n";
+	}
 
-  my @no_index = $self->no_index;
-  if (@no_index) {
-    say "no_index directory qw{ @no_index }";
-    print "\n";
-  }
+	my @no_index = $self->no_index;
+	if (@no_index) {
+		print "no_index directory qw{ @no_index }\n\n";
+	}
 
-  #	print "\n";
-
-  return;
+	return;
 }
 
 no Moo;
